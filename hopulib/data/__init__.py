4d6178313beb7426479a19251a6152bae2c1f03d
mahaloz <zbasque@asu.edu>
2022-08-20 18:27:01 -0700

    templates

--- hopulib/data/__init__.py
+++ hopulib/data/__init__.py
@@ -1,5 +1,5 @@
 from typing import List, Generator
-
+import itertools
 
 def split_into_n_parts(lst: List, n: int) -> Generator:
     """
@@ -12,3 +12,15 @@ def split_into_n_parts(lst: List, n: int) -> Generator:
     k, m = divmod(len(lst), n)
     return (lst[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
 
+def n_combinations(lst: List, n: int):
+    lists = [
+        lst for _ in range(n)
+    ]
+    return itertools.product(*lists)
+
+def n_byte_combinations(n: int):
+    lst = [b for b in range(256)]
+    return n_combinations(lst, n)
+
+def n_ascii_combinations():
+    pass
--- hopulib/templates/pwn_template.py
+++ hopulib/templates/pwn_template.py
@@ -1,2 +1,61 @@
-import angr
+#!/usr/bin/python3
+from pwn import *
+import re
+import sys
+import itertools
 
+# connection parameter setup
+HOST, PORT = '192.168.0.1', 1337
+context.terminal = ["tmux", "splitw", "-h"]
+context.update(log_level='debug')
+
+# run parameters
+DEBUG = True
+PRELOAD = False
+
+def main():
+    args = ["./binary"]
+    if (PRELOAD):
+        libc_loc = "./libc"
+        p = process(args, env={"LD_PRELOAD": libc_loc}
+    else:
+        p = process(args)
+    else:
+            p = remote(HOST, PORT)
+
+            # start pwn process
+            proc = PWNProcess(p)
+
+    # Useful commands:
+    # ============================
+    #
+    #   LIBC & ELF Manipulation
+    #   =======================
+    #   libc = ELF("./libc-generic.so")
+    #   elf = ELF("./generic_binary")
+    #   elf.got['function'] #-> returns addr
+    #   system = libc.symbols['system'] #-> returns addr of symbol
+    #   binsh = libc.search('/bin/sh').next() #-> returns addr of str
+    #
+    #   Proccess Manip & DBG
+    #   =======================
+    #   gdb.attach(p, '''   #attach to the proc, p
+    #       set follow-fork-mode child
+    #       break *0x08048099
+    #       continue
+    #       ''')
+    #
+    #   p.sendline(line)
+    #   p.recv(1024)
+    #   p.revcuntil("thing")
+    #   p.sendlineafter("recved", "wanttosend")
+    #
+    #   Pretty Printing & Bytes
+    #   =======================
+    #   p64(0xdeadbeef) #int to bytes
+    #   u64("\xef\xbe\xad\xde") #bytes to int
+    #   log.success("string")
+    #   log.info("string")
+
+if __name__ == '__main__':
+    main()
